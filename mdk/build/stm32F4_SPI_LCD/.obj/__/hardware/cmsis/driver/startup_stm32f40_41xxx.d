f15a2a73a60976707c140942b48d4c8e7900dda4
吴奕鹏 <2838531761@qq.com>
2025-12-22 14:30:48 +0800

    删掉仓库中的编译文件

--- mdk/build/stm32F4_SPI_LCD/.obj/__/hardware/cmsis/driver/startup_stm32f40_41xxx.d
+++
@@ -1,1 +0,0 @@
-.\build\stm32F4_SPI_LCD\.obj\__\hardware\cmsis\driver\startup_stm32f40_41xxx.o: .\..\hardware\cmsis\driver\startup_stm32f40_41xxx.s
--- mdk/build/stm32F4_SPI_LCD/.obj/__/third_lib/OLED_Data.d
+++
@@ -1,2 +0,0 @@
-.\build\stm32F4_SPI_LCD\.obj\__\third_lib\OLED_Data.o: .\..\third_lib\OLED_Data.c
-.\build\stm32F4_SPI_LCD\.obj\__\third_lib\OLED_Data.o: .\..\third_lib\OLED_Data.h
